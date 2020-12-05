0bc8a727327e9339e88db5485c871e1f94a6e532
jy6c9w08 <yzc727824984@gmail.com>
2020-12-05 20:55:03 +0800

    添加瀑布流路由

--- lib/get_route.dart
+++ lib/get_route.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 import 'package:jy6c9w08_flutter/page/button_list.dart';
 import 'package:jy6c9w08_flutter/page/snow_man.dart';
+import 'package:jy6c9w08_flutter/page/waterfall_flow.dart';
 
 List<GetPage> routeList = [
   GetPage(name: '/', page: () => ButtonList()),
+  GetPage(name: '/waterfall_flow', page: () => WaterfallFlowPage()),
   GetPage(name: '/snow_man', page: () => SnowMan()),
 ];
